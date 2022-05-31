c6b6143e3cc9b5070332069e6355767a08c9b4a3
MaksymBezrodnyiTR <Maksym.Bezrodnyi@thomsonreuters.com>
2022-05-31 14:39:04 +0300

    done: create.js (1)

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,17 @@
+import { readdir, writeFile } from "node:fs/promises";
+
 export const create = async () => {
-    // Write your code here 
+  const pathToFolder = "./src/fs/files";
+  const fileName = "fresh.txt";
+  const text = "I am fresh and young";
+  const errorMessage = "FS operation failed";
+
+  try {
+    const files = await readdir(pathToFolder);
+    if (files.includes(fileName)) throw Error(errorMessage);
+    await writeFile(`${pathToFolder}/${fileName}`, text);
+    console.log(`The file '${fileName}' has been saved!`);
+  } catch (e) {
+    console.log(e.message);
+  }
 };
