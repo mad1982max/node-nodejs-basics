f4ad3df11aadd521ba47ab10d1bb0c7e41ccb1bd
MaksymBezrodnyiTR <Maksym.Bezrodnyi@thomsonreuters.com>
2022-06-04 00:47:46 +0300

    done: env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,9 @@
 export const parseEnv = () => {
-    // Write your code here 
+  const prefix = "RSS_";
+  const parsedRSSString = Object.entries(process.env)
+    .filter(([key]) => key.startsWith(prefix))
+    .map(([key, value]) => `${key}=${value}`)
+    .join(";");
+
+  console.log(parsedRSSString);
 };
