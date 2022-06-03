7537ea54ccc58b6f95aae127cf1fd51ccf12aa06
MaksymBezrodnyiTR <Maksym.Bezrodnyi@thomsonreuters.com>
2022-06-03 22:37:17 +0300

    done: delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,17 @@
+import { unlink } from "fs";
+import path from "path";
+
 export const remove = async () => {
-    // Write your code here 
+  const fileToRemove = "fileToRemove.txt";
+  const pathToRecourseFolder = "./src/fs/files";
+  const pathToFile = path.join(pathToRecourseFolder, fileToRemove);
+  const errorMessage = "FS operation failed";
+
+  try {
+    unlink(pathToFile, (err) => {
+      if (err) throw Error(errorMessage);
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
 };
