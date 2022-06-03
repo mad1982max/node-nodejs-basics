7913bf3f558a3743c398df1bd5d15e4b4111d246
MaksymBezrodnyiTR <Maksym.Bezrodnyi@thomsonreuters.com>
2022-06-03 18:24:51 +0300

    fixes: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,15 +1,20 @@
 import { readdir, writeFile } from "node:fs/promises";
+import path from "path";
 
 export const create = async () => {
-  const pathToFolder = "./src/fs/files";
+  const pathToRecourseFolder = "./src/fs";
+  const recourseFolder = "files";
   const fileName = "fresh.txt";
   const text = "I am fresh and young";
   const errorMessage = "FS operation failed";
 
+  const recoursePath = path.join(pathToRecourseFolder, recourseFolder);
+  const targetPath = path.join(recoursePath, fileName);
+
   try {
-    const files = await readdir(pathToFolder);
+    const files = await readdir(recoursePath);
     if (files.includes(fileName)) throw Error(errorMessage);
-    await writeFile(`${pathToFolder}/${fileName}`, text);
+    await writeFile(targetPath, text);
     console.log(`The file '${fileName}' has been saved!`);
   } catch (e) {
     console.log(e.message);
