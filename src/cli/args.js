e268fd1e9d7352fa1560338eb9c946b4d2c72d98
MaksymBezrodnyiTR <Maksym.Bezrodnyi@thomsonreuters.com>
2022-06-04 01:08:41 +0300

    done: args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,14 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const chunkSize = 2;
+  const args = process.argv.slice(2);
+  const result = [];
+
+  while (args.length) {
+    const chunk = args.splice(0, chunkSize);
+    result.push(chunk);
+  }
+
+  for (const [key, value] of result) {
+    console.log(`${key.slice(2)} is ${value}`);
+  }
 };
