b343e6ae9a3815f3eb96a8f69d421c522c939caa
MaksymBezrodnyiTR <Maksym.Bezrodnyi@thomsonreuters.com>
2022-06-03 18:20:02 +0300

    done: copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,45 @@
+import { readdir, lstat, mkdir, copyFile } from "node:fs/promises";
+import path from "path";
+
 export const copy = async () => {
-    // Write your code here 
+  const pathToRecourseFolder = "./src/fs";
+  const recourseFolder = "files";
+  const targetFolder = "files_copy";
+  const errorMessage = "FS operation failed";
+  const recoursePath = path.join(pathToRecourseFolder, recourseFolder);
+  const targetPath = path.join(pathToRecourseFolder, targetFolder);
+  let folderContent;
+
+  //for copying file and folders recursively
+  const readAndCopy = async (pathToOldFolder, pathToNewFolder) => {
+    const content = await readdir(pathToOldFolder);
+    for (const item of content) {
+      const pathToOldItem = path.join(pathToOldFolder, item);
+      const pathToNewItem = path.join(pathToNewFolder, item);
+
+      const itemInfo = await lstat(pathToOldItem);
+
+      if (itemInfo.isFile()) {
+        await copyFile(pathToOldItem, pathToNewItem);
+      } else {
+        //for folders
+        await mkdir(pathToNewItem);
+        readAndCopy(pathToOldItem, pathToNewItem);
+      }
+    }
+  };
+
+  try {
+    try {
+      await mkdir(path.join(pathToRecourseFolder, targetFolder));
+      folderContent = await readdir(recoursePath);
+    } catch (e) {
+      throw Error(errorMessage);
+    }
+    readAndCopy(recoursePath, targetPath);
+  } catch (e) {
+    console.log(e.message);
+  }
 };
+
+copy();
