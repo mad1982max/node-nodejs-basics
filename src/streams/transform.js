eb601cbe38bd24ccf3f1999d032c359c37921b17
MaksymBezrodnyiTR <Maksym.Bezrodnyi@thomsonreuters.com>
2022-06-04 23:58:17 +0300

    done: transform.js (streams)

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,12 @@
+import { Transform } from "stream";
+
 export const transform = async () => {
-    // Write your code here 
+  const reverseTransStream = new Transform({
+    transform(chunk, encoding, cb) {
+      this.push(chunk.toString().split("").reverse().join("") + "\n");
+      cb();
+    },
+  });
+
+  process.stdin.pipe(reverseTransStream).pipe(process.stdout);
 };
