9f801f9f7d79f0848f01bdd6fa7c04064625a498
MaksymBezrodnyiTR <Maksym.Bezrodnyi@thomsonreuters.com>
2022-06-04 23:37:37 +0300

    done: write.js (streams)

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,16 @@
+import path from "path";
+import { createWriteStream } from "fs";
+
 export const write = async () => {
-    // Write your code here 
+  const pathToFolder = "./src/streams/files";
+  const txtFile = "fileToWrite.txt";
+  const pathToFile = path.join(pathToFolder, txtFile);
+
+  const writeStream = createWriteStream(pathToFile, { encoding: "utf8" });
+
+  process.stdin
+    .on("error", (err) => {
+      console.log(err.message);
+    })
+    .pipe(writeStream);
 };
