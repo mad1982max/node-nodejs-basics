183ad5dcf191332b4661173b1c2c53043a0a9575
MaksymBezrodnyiTR <Maksym.Bezrodnyi@thomsonreuters.com>
2022-06-04 23:19:19 +0300

    done: read.js (streams)

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,16 @@
+import { createReadStream } from "fs";
+import path from "path";
+
 export const read = async () => {
-    // Write your code here 
+  const pathToFolder = "./src/streams/files";
+  const txtFile = "fileToRead.txt";
+  const pathToFile = path.join(pathToFolder, txtFile);
+
+  const readableStream = createReadStream(pathToFile, { encoding: "utf8" });
+
+  readableStream
+    .on("error", function (error) {
+      console.log(`error: ${error.message}`);
+    })
+    .pipe(process.stdout);
 };
