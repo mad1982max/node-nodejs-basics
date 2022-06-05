6cbd440bd3e598a5b1101ffe7d226e859db1e99e
MaksymBezrodnyiTR <Maksym.Bezrodnyi@thomsonreuters.com>
2022-06-05 20:39:03 +0300

    done: main.js + fixes in workers.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,42 @@
+import os from "os";
+import { Worker } from "worker_threads";
+
 export const performCalculations = async () => {
-    // Write your code here
+  let start = 10;
+  const cpuCores = os.cpus().length;
+  const filePath = "./src/wt/worker.js";
+  const results = [];
+  const workers = [];
+  let remainWorkers = 0;
+
+  for (let i = 0; i < cpuCores; i++) {
+    const worker = new Worker(filePath);
+    worker.on("message", (data) => {
+      results.push({
+        status: "resolved",
+        data,
+      });
+      worker.terminate();
+    });
+    worker.on("error", (err) => {
+      results.push({
+        status: "error",
+        data: null,
+      });
+    });
+    worker.on("exit", (code) => {
+      if (code != 0) {
+        remainWorkers--;
+        if (remainWorkers === 0) {
+          console.log({ results });
+        }
+      }
+    });
+    workers.push(worker);
+    remainWorkers = workers.length;
+  }
+
+  for (const worker of workers) {
+    worker.postMessage(start++);
+  }
 };
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -2,10 +2,10 @@ import { parentPort } from "worker_threads";
 
 export const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
 
-export const sendResult = () => {
+export const sendResult = (() => {
   parentPort.on("message", (data) => {
-    console.log("data from main: ", data);
     const result = nthFibonacci(data);
+    console.log(`in worker: ${data}nthFibonacci is ${result}`);
     parentPort.postMessage(result);
   });
-};
+})();
