0e9765f3a48f9320b861534035ab91194b02a187
MaksymBezrodnyiTR <Maksym.Bezrodnyi@thomsonreuters.com>
2022-06-05 14:20:29 +0300

    done: decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,26 @@
+import { createUnzip } from "zlib";
+import { createReadStream, createWriteStream } from "fs";
+import path from "path";
+
 export const decompress = async () => {
-    // Write your code here 
+  const pathToFolder = "./src/zip/files";
+  const targetFile = "fileToCompress.txt";
+  const resourceFile = "archive.gz";
+  const pathToResourceFile = path.join(pathToFolder, resourceFile);
+  const pathToTargetFile = path.join(pathToFolder, targetFile);
+
+  const unzip = createUnzip();
+  const input = createReadStream(pathToResourceFile);
+  const output = createWriteStream(pathToTargetFile);
+
+  const handleError = (err) => {
+    console.log(err.message);
+  };
+
+  input
+    .on("error", handleError)
+    .pipe(unzip)
+    .on("error", handleError)
+    .pipe(output)
+    .on("error", handleError);
 };
